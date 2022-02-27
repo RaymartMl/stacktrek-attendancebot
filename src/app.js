29c02bc91330bdb15cde3b98ab8a4f02e7fd13a9
RaymartMl <38749661+RaymartMl@users.noreply.github.com>
2022-02-27 14:38:57 +0800

    Bootstrap a slack bot

---
+++ src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
+
+app.message("hello", async ({ message, say }) => {
+  await say({
+    text: `Hey there <@${message.user}>!`,
+  });
+});

fc2b2b291378361ccdb14891e6d8d8c9a3b20e14
RaymartMl <38749661+RaymartMl@users.noreply.github.com>
2022-03-06 14:10:38 +0800

    improve how listeners is used

--- src/listeners.js
+++ src/listeners.js
@@ -1,12 +1,11 @@
 import { getInfo, emojiReaction, getMentionUserIds } from "./utils.js";
 import { timeIn, timeOut } from "./GoogleSpreadSheet.js";
-import { onlyDixiBot, onlyTimeInTimeOut } from "./middlewares.js";
+import { onlyDixiBot } from "./middlewares.js";
 import { subtype } from "@slack/bolt";
 
 export function registerListeners(app) {
-  app.message(subtype("bot_message"), onlyDixiBot("Time-In"), timeInCallback);
-  // app.message("Time-Out", onlyDixiBot, onlyTimeInTimeOut, timeOutCallback);
-  // app.message("hello", ({ message }) => console.log(message));
+  app.message(subtype("bot_message"), onlyDixiBot("Time-in"), timeInCallback);
+  app.message(subtype("bot_message"), onlyDixiBot("Time-out"), timeOutCallback);
 }
 
 async function timeInCallback({ message, client, logger }) {
@@ -15,6 +14,7 @@ async function timeInCallback({ message, client, logger }) {
     const { name, date, time } = await getInfo(userIds[0], message, client);
 
     timeIn(name, date, time);
+    await emojiReaction("thumbsup", client, message);
   } catch (error) {
     logger.error(error);
   }
@@ -26,6 +26,7 @@ async function timeOutCallback({ message, client, logger }) {
     const { name, date, time } = await getInfo(userIds[0], message, client);
 
     timeOut(name, date, time);
+    await emojiReaction("thumbsup", client, message);
   } catch (error) {
     logger.error(error);
   }

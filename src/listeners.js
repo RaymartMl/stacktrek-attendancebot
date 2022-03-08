9ab6cfde12bb56dd7dad35ad823b3833e81af7ab
RaymartMl <38749661+RaymartMl@users.noreply.github.com>
2022-03-08 11:28:31 +0800

    make sure that time in is properly handled before reacting with emojis

--- src/listeners.js
+++ src/listeners.js
@@ -1,12 +1,19 @@
 import { getInfo, emojiReaction, getMentionUserIds } from "./utils.js";
-import { timeIn, timeOut } from "./GoogleSpreadSheet.js";
+import {
+  TimedInError,
+  TimedOutError,
+  timeIn,
+  timeOut,
+} from "./GoogleSpreadSheet.js";
 import { onlyDixiBot } from "./middlewares.js";
 import { subtype } from "@slack/bolt";
+import { debugListeners } from "./debugListeners.js";
 
 export function registerListeners(app) {
   app.message(subtype("bot_message"), onlyDixiBot("Time-in"), timeInCallback);
   app.message(subtype("bot_message"), onlyDixiBot("Time-out"), timeOutCallback);
-  app.message("debug", debugCallback);
+
+  // debugListeners(app);
 }
 
 async function timeInCallback({ message, client, logger }) {
@@ -14,10 +21,13 @@ async function timeInCallback({ message, client, logger }) {
     const userIds = getMentionUserIds(message.attachments);
     const { name, date, time } = await getInfo(userIds[0], message, client);
 
-    timeIn(name, date, time);
+    await timeIn(name, date, time);
     await emojiReaction("thumbsup", client, message);
   } catch (error) {
     logger.error(error);
+    if (error instanceof TimedInError) {
+      await emojiReaction("x", client, message);
+    }
   }
 }
 
@@ -26,20 +36,12 @@ async function timeOutCallback({ message, client, logger }) {
     const userIds = getMentionUserIds(message.attachments);
     const { name, date, time } = await getInfo(userIds[0], message, client);
 
-    timeOut(name, date, time);
+    await timeOut(name, date, time);
     await emojiReaction("thumbsup", client, message);
   } catch (error) {
     logger.error(error);
-  }
-}
-
-async function debugCallback({ message, client, logger }) {
-  try {
-    const { name, date, time } = await getInfo(message.user, message, client);
-
-    timeIn(name, date, time);
-    await emojiReaction("thumbsup", client, message);
-  } catch (error) {
-    logger.error(error);
+    if (error instanceof TimedInError || error instanceof TimedOutError) {
+      await emojiReaction("x", client, message);
+    }
   }
 }

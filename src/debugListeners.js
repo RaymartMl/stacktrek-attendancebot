deb98739e67c72d63a8f0a581add758d8920f2d3
RaymartMl <38749661+RaymartMl@users.noreply.github.com>
2022-03-08 14:51:07 +0800

    fix import bug

--- src/debugListeners.js
+++ src/debugListeners.js
@@ -1,5 +1,5 @@
-import { timeIn, TimedInError } from "./GoogleSpreadSheet";
-import { emojiReaction, getInfo } from "./utils";
+import { timeIn, TimedInError } from "./GoogleSpreadSheet.js";
+import { emojiReaction, getInfo } from "./utils.js";
 
 export function debugListeners(app) {
   app.message("normal-debug", normalDebugCallback);

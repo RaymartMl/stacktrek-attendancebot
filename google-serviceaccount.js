65eb8bdb4d629e08569df3bd43c765d99fcd7e97
RaymartMl <38749661+RaymartMl@users.noreply.github.com>
2022-03-08 19:14:46 +0800

    test

--- google-serviceaccount.js
+++ google-serviceaccount.js
@@ -22,5 +22,5 @@ const jsonFile = JSON.stringify(
 
 const unEscapedJsonFile = jsonFile.replace(/\\n/g, "n");
 // console.log("Hello =====================================", import.meta.url);
-writeFileSync("/tmp/google-serviceaccount.json", unEscapedJsonFile);
+writeFileSync("/var/task/google-serviceaccount.json", unEscapedJsonFile);
 console.log("=====================================", import.meta.url);
